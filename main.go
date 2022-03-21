242e2f57430fb3e1ca21f8572a5688b3a3ae5ee7
XuUSTC <54992446+XuuuUSTC@users.noreply.github.com>
2022-03-21 16:26:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-
+//main.go
+package main
+ 
+import "fmt"
+ 
+func main() {
+  var cmd string
+  for {
+      fmt.Print("请输入命令(Q退出)：")
+      n, _ := fmt.Scanln(&cmd)
+      if n == 0 {
+          continue
+      }
+      if cmd == "Q" || cmd == "q" {
+          break
+      }
+      switch cmd {
+      case "help":
+          fmt.Println("do something about help")
+      case "list":
+          fmt.Println("do something about list")
+      default:
+          fmt.Println("sorry, The command \"" + cmd + "\" is not defined")
+      }
+  }
+}
